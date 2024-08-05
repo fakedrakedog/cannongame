2ff15e5658b7bfd4608be5acd40efc48bf578e1c
fakedrakedog <kkn8800@naver.com>
2024-08-05 15:40:34 +0000

    ball.java

---
+++ src/main/java/com/cannongame/Ball.java
@@ -0,0 +1,22 @@
+package com.cannongame;
+
+public class Ball {
+    private int x;
+    private int y;
+    private int radious;
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public int getRadious() {
+        return radious;
+    }
+    public Ball(int x, int y, int radious) {
+        this.x = x;
+        this.y = y;
+        this.radious = radious;
+    }
+
+}
